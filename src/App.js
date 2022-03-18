2b394da18ff7ed868beb2f642186c88bc92564ae
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-03-18 13:49:14 +0530

    add sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./categories.styles.scss";
+
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+  },
+  {
+    id: 4,
+    title: "Womens",
+  },
+  {
+    id: 5,
+    title: "Mens",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>SHOP NOW</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
