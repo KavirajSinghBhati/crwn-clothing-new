d9f32d5dde51f152dfc7eac0ec738a88cc3ae445
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-04-17 17:07:06 +0530

    directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React from "react";
-
-import "./categories.styles.scss";
-
-const categories = [
-  {
-    id: 1,
-    title: "Hats",
-  },
-  {
-    id: 2,
-    title: "Jackets",
-  },
-  {
-    id: 3,
-    title: "Sneakers",
-  },
-  {
-    id: 4,
-    title: "Womens",
-  },
-  {
-    id: 5,
-    title: "Mens",
-  },
-];
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>SHOP NOW</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory />;
 };
 
 export default App;
